e229135c22833bb8393989d7c401899d0fdc43da
kw170 <kailuwilliams@gmail.com>
2023-07-03 12:12:19 -0700

    Add upcoming content

--- src/nav/upcoming.js
+++ src/nav/upcoming.js
@@ -1,7 +1,25 @@
 
+function createUpcomingUpper(){
+    const upcomingUpper = document.createElement('div')
+    upcomingUpper.classList.add('upcomingUpper')
 
+    const date = new Date().toUTCString().slice(7, 16);
+    const dateTitle = document.createElement('div')
+    dateTitle.classList.add('dateTitle')
+    dateTitle.textContent = date
+    upcomingUpper.appendChild(dateTitle)
+
+    return upcomingUpper
+}
 function loadUpcoming(){
+    const upcomingContent = document.createElement('div')
+    upcomingContent.classList.add('upcomingContent')
 
+    const upcomingUpper = createUpcomingUpper()
+
+    upcomingContent.appendChild(upcomingUpper)
+
+    return upcomingContent
 }
 
 export default loadUpcoming;
