cd9107b0d423e8d1a1f82ee8ba360bb4c74ecf9a
kw170 <kailuwilliams@gmail.com>
2023-07-03 14:42:51 -0700

    Add event listener to add task button

--- src/nav/inbox.js
+++ src/nav/inbox.js
@@ -24,6 +24,7 @@ function createInboxLower(){
     addTaskBox.classList.add('addTaskBox')
     inboxLower.appendChild(addTaskBox)
 
+
     const plusIcon = document.createElement('span') 
     plusIcon.classList.add("plusIcon")
     plusIcon.innerHTML = "<img src ='../imgs/plus.png'>"
@@ -33,6 +34,54 @@ function createInboxLower(){
 
     addTaskBox.appendChild(plusIcon)
     addTaskBox.appendChild(addTaskText)
+
+    // Event listener
+    addTaskBox.addEventListener('click',()=>{
+        const form = document.createElement('form')
+        form.classList.add("form")
+
+        const input = document.createElement('div')
+        input.classList.add("input")
+        form.appendChild(input)
+
+        const submit = document.createElement('div')
+        submit.classList.add("submit")
+        form.appendChild(submit)
+
+        const taskName = document.createElement('input')
+        taskName.classList.add('taskName')
+        taskName.setAttribute("type", "text")
+        taskName.setAttribute("name", taskName)
+        taskName.setAttribute("placeholder", "Task name")
+        taskName.required = true
+
+        const taskDescription = document.createElement('input')
+        taskDescription.classList.add('taskDescription')
+        taskDescription.setAttribute("type", "text")
+        taskDescription.setAttribute("name", taskDescription)
+        taskDescription.setAttribute("placeholder", "Description")
+        taskDescription.required = true
+
+        const dueDate = document.createElement('input')
+        dueDate.classList.add('dueDate')
+        dueDate.setAttribute("type", "date")
+        dueDate.setAttribute("name", dueDate)
+        dueDate.required = true
+
+        const add = document.createElement("input");
+        add.classList.add("add");
+        add.setAttribute("type", "image");
+        add.setAttribute("src", "../imgs/check.png");
+        add.setAttribute("alt", "Add");
+
+        input.append(taskName)
+        input.append(taskDescription)
+        input.append(dueDate)
+        submit.append(add)
+
+
+        inboxLower.prepend(form)
+    })
     return inboxLower
 }
 
